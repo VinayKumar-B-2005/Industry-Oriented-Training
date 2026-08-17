6af4d146815048aaa07b25d00354b007694bf7b9
Vinay Kumar B <this.is.vinaykumar.b@gmail.com>
2026-08-17 18:57:02 +0530

    Add circle method to calculate area of a circle

---
+++ Day-1/p7.java
@@ -0,0 +1,13 @@
+public class p7 {
+	void circle(float r) {
+		double res=3.14*r*r;
+		System.out.println(res);
+		
+	}
+	public static void main(String[] args) {
+		p7 area=new p7();
+		area.circle(6);
+
+	}
+
+}
