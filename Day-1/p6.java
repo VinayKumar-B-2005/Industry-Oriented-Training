5b971c5d40ceee41137bc532fc934cf67697dc4b
Vinay Kumar B <this.is.vinaykumar.b@gmail.com>
2026-08-17 18:55:15 +0530

    Add palindrome checker in p6.java

---
+++ Day-1/p6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class p6 {
+	void palindrome(String s,String rev) {
+		for(int i=s.length()-1;i>=0;i--) {
+			rev=rev+s.charAt(i);
+		}
+		System.out.println("Palindrome of "+s+" is:"+rev);
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String rev="";
+		p6 pal=new p6();
+		pal.palindrome(s, rev);
+	}
+
+}
