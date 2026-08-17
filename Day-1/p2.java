b8545760e38611ba6ef4950425ede03f3b45bcef
Vinay Kumar B <this.is.vinaykumar.b@gmail.com>
2026-08-17 17:05:17 +0530

    Add p2 class with subtraction method

---
+++ Day-1/p2.java
@@ -0,0 +1,12 @@
+public class p2 {
+	void sub(int a,int b) {
+		System.out.println(a-b);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		p2 calc=new p2();
+		calc.sub(4,2);
+	}
+
+}
