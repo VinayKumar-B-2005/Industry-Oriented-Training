a51a1d45a1079b2306b6dcc926e5896008cf61d2
Vinay Kumar B <this.is.vinaykumar.b@gmail.com>
2026-08-17 18:58:23 +0530

    Add triangle method to calculate area of triangle

---
+++ Day-1/p8.java
@@ -0,0 +1,12 @@
+public class p8 {
+	void triangle(float b,float h) {
+		double res=1/2*b*h;
+		System.out.println(res);
+	}
+	public static void main(String[] args) {
+		p8 area=new p8();
+		area.triangle(1,5);
+
+	}
+
+}
