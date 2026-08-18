5ddccc5027fcb14067e9f097ba1bddaaa4d8de5b
Vinay Kumar B <this.is.vinaykumar.b@gmail.com>
2026-08-18 16:11:45 +0530

    Fix palindrome check and output message

--- Day-1/p6.java
+++ Day-1/p6.java
@@ -5,7 +5,11 @@ public class p6 {
 		for(int i=s.length()-1;i>=0;i--) {
 			rev=rev+s.charAt(i);
 		}
-		System.out.println("Palindrome of "+s+" is:"+rev);
+		if(s.equals(rev)) {
+		System.out.println("Palindrome of "+s+" is:"+rev);}
+		else {
+			System.out.println("No Palindrome");
+		}
 	}
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
