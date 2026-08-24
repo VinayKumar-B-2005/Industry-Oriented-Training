b66e09a258341445209eb460e80a731d225c3c15
Vinay Kumar B <this.is.vinaykumar.b@gmail.com>
2026-08-24 16:43:28 +0530

    Add Java program to concatenate and print names
    Prints the name and concatenated name to the console.

---
+++ Day-5/p1.java
@@ -0,0 +1,12 @@
+package day_5;
+
+public class p1 {
+	public static void main(String[] args) {
+		String name="Vinay";
+		String s1=name.concat(" Kumar");
+		System.out.println(name);
+		System.out.println(s1);
+		
+	}
+}
+
